2e886403f9df5617a1529b8cb183752d77202f3f
kocmoc-u <coened.beef@gmail.com>
2021-07-10 00:54:18 +0900

    scripts/hello.js を修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,18 +2,18 @@
 module.exports = robot => {
   robot.hear(/Привет/i, msg => {
     const user_id = msg.message.user.id;
-    msg.send(`<@${user_id}> Привет`);
+    msg.send(`Привет! Товарищ <@${user_id}>`);
   });
   robot.hear(/昼食/i, msg => {
     const user_id = msg.message.user.id;
     const lots = ['コンビニ', '吉野家', 'はしご', 'はなまるうどん', '行ったことのない店'];
     const lot = lots[Math.floor(Math.random() * lots.length)];
-    msg.send(`<@${user_id}> 昼食は ${lot} が良かろう`);
+    msg.send(`<@${user_id}> 昼食は${lot}が良かろう`);
   });
   robot.hear(/おみくじ/i, msg => {
     const user_id = msg.message.user.id;
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
     const lot = lots[Math.floor(Math.random() * lots.length)];
-    msg.send(`<@${user_id}> 今日の運勢は ${lot}`);
+    msg.send(`<@${user_id}> 今日の運勢は${lot}です`);
   });
 };
